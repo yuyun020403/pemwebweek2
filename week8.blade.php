9bb6b64c8cb817ba09edadd5bfe571f8e60016a1
Yuyun trisma hartati <114551336+yuyun020403@users.noreply.github.com>
2023-05-22 09:45:06 +0700

    Add files via upload

---
+++ week8.blade.php
@@ -0,0 +1,62 @@
+<h1 style="text-align: center; color:black; font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">Data Diri</h1>
+
+<div style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">
+<form >
+<label for="name">Nama :</label><br>
+  <input type="text" id="name" name="name"><br>
+  <label for="tgl">Tgl Pemeriksaan :</label><br>
+  <input type="date" id="tgl" name="tgl"><br>
+  <label for="tgl_lahir">Tgl lahir :</label><br>
+  <input type="date" id="tgl_lahir" name="tgl_lahir"><br>
+  <label for="umur">Usia :</label><br>
+  <input type="text" id="umur" name="umur"><br>
+  <label for="name">Jenis kelamin :</label><br>
+  <input type="text" id="jk" name="jk"><br>
+</form>
+</div>
+
+<table  border="1"cellspacing="0" cellpadding="5" align="center" width="800" style="font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; " >
+    <tr>
+        <th>Jenis tes</th>
+        <th>Hasil Pemeriksaan</th>
+        <th>Normal</th>
+    </tr>
+    <tr>
+        <td>
+            Tekanan darah
+        </td>
+        <td>
+            
+        </td>
+        <td>
+            120/80 mmhg
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Asam Urat
+        </td>
+        <td>
+           
+        </td>
+        <td>
+        Pria : < 7 mg/dl | Wanita: < 6 mg/dl
+        </td>
+    </tr>
+    <tr>
+        <td>kolestrol total</td>
+        <td></td>
+        <td>< 200 mg/dl</td>
+    </tr>
+    <tr>
+        <td>Gula darah</td>
+        <td></td>
+        <td>
+            Puasa: 70-110 mg/dl
+            <br>
+            2 jam setelah makan: 100-150 mg/dl
+            <br>
+            Sewaktu/acak : 70-125 mg/dl
+        </td>
+    </tr>
+</table>
